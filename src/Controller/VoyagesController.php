ec9e598145d5a4b4100a44bed7d90119f37bad61
Kargos <Kargo@DESKTOP-5U2TK3N>
2021-12-21 21:05:03 +0100

    Phase 4 : plusieurs pages

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Description of VoyagesController
+ *
+ * @author Kargo
+ */
+class VoyagesController extends AbstractController{
+    
+    /**
+     * @Route("voyages", name="voyages")
+     * @return Response
+     */
+    public function index(): Response{
+        return $this->render("pages/voyages.html.twig");
+    }
+}
