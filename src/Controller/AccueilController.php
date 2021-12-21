6b43dea5d3384aa6ea9eade56f64bdf45254632f
Kargos <Kargo@DESKTOP-5U2TK3N>
2021-12-21 14:37:12 +0100

    Phase 3 : introduction à Twig et simplificagion du contrôleur

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,18 +9,20 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Description of AccueilController
  *
- * @author Kargo
+ * @author Kargos
  */
-class AccueilController {
+class AccueilController extends AbstractController {
+    
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
-    public function index():Response{
-        return new Response ('Hello world !');
+    public function index(): Response{
+        return $this->render("pages/accueil.html.twig");
     }
 }
